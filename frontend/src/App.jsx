40c7ad92a0875bfc90a9d048d81e46e7d8204b1c
FernandoVargas15 <luis.vargas89@unach.mx>
2025-10-10 19:44:14 -0600

    Modificaciones en el app.js (rutas)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,27 @@
-// src/App.jsx
+import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginForm from "./functionalities/LoginForm.jsx";
-import MiHorario from "./functionalities/Profesor/MiHorario.jsx"; 
+import LoginForm from "./functionalities/LoginForm";
+import RecoveryForm from "./functionalities/RecoveryForm";
+import MiHorario from "./functionalities/Profesor/MiHorario.jsx";
 import AdminDashboard from "./functionalities/Administrador/Dashboard.jsx";
+import "./App.css";
 
-export default function App() {
+function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<LoginForm />} />
+        <Route path="/Login" element={<LoginForm />} />     
+        <Route path="/Recovery" element={<RecoveryForm />} />
+        
         {/* Profesor */}
         <Route path="/profesor/mi-horario" element={<MiHorario />} />
+
         {/* Admin */}
-        <Route path="/admin/admin-dashboard" element={<AdminDashboard />} /> 
+        <Route path="/admin/admin-dashboard" element={<AdminDashboard />} />
       </Routes>
     </BrowserRouter>
   );
 }
+
+export default App;
